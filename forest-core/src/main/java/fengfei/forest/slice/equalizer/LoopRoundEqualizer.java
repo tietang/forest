13f858ea126532bb456f0b2f3afcbdeb21b7a3aa
Tietang Wang <wtt.2012@gmail.com>
2013-04-11 17:35:28 +0800

    modified:   forest-core/pom.xml

--- forest-core/src/main/java/fengfei/forest/slice/equalizer/LoopRoundEqualizer.java
+++ forest-core/src/main/java/fengfei/forest/slice/equalizer/LoopRoundEqualizer.java
@@ -1,50 +1,49 @@
 package fengfei.forest.slice.equalizer;
 
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Random;
 import java.util.Set;
-import java.util.Map.Entry;
 
 import org.apache.commons.collections.MultiMap;
 import org.apache.commons.collections.map.MultiValueMap;
 
 import fengfei.forest.slice.Equalizer;
 
-public class RoundLongEqualizer implements Equalizer<Long> {
+public class LoopRoundEqualizer implements Equalizer<Long> {
 
-	private long maxKey = 2000000;
+	private long moduleSize = 1024;
 
-	public RoundLongEqualizer(long maxKey) {
+	public LoopRoundEqualizer() {
+	}
+
+	public LoopRoundEqualizer(long moduleSize) {
 		super();
-		this.maxKey = maxKey;
+		this.moduleSize = moduleSize;
 	}
 
-	public long getMaxKey() {
-		return maxKey;
-	}
-
-	public void setMaxKey(long maxKey) {
-		this.maxKey = maxKey;
+	public long getModuleSize() {
+		return moduleSize;
 	}
 
 	@Override
 	public long get(Long key, int sliceSize) {
-		long maxSize = maxKey * sliceSize;
-		long index = Math.abs(key % maxSize);
-		return index == 0 ? maxSize : index;
+		long module = key % moduleSize;
+		long index = Math.abs(module % sliceSize);
+		return index == 0 ? sliceSize : index;
 	}
 
 	public static void main(String[] args) {
 		int size = 60;
 		MultiMap map = new MultiValueMap();
 		Random random = new Random();
-		RoundLongEqualizer e = new RoundLongEqualizer(1024);
+		LoopRoundEqualizer e = new LoopRoundEqualizer(1024);
 		for (int i = 1; i <= 1000000; i++) {
 			long key = i;
 			key = random.nextLong();
 			long index = e.get(key, size);
 			map.put(index, key);
-			// System.out.println(i + " : " + index);
+//			System.out.println(i + " : " + index);
 			// System.out.println(i + " : " + e.get(random.nextLong(), 3));
 		}
 		Set<Entry> set = map.entrySet();
